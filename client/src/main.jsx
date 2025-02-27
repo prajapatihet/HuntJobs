f20d2bda267410f9c0d3aba9f9a6a23a7c3f5674
Hetkumar Prajapati <hetgoraj@gmail.com>
2025-02-27 22:14:32 +0530

    clean-up

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,6 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
-fetch('/api/v1/test')
-  .then((res) => res.json())
-  .then((data) => console.log(data));
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
