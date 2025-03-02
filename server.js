4901898b5a7db20f2e4fd5b1afb16c40fff03832
Hetkumar Prajapati <hetgoraj@gmail.com>
2025-03-02 13:28:20 +0530

    fixed

--- server.js
+++ server.js
@@ -1,29 +1,12 @@
 import 'express-async-errors';
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
-
+import * as dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 const app = express();
-app.use(express.json());
-app.use(cookieParser());
-
-import * as dotenv from 'dotenv';
 import morgan from 'morgan';
-dotenv.config();
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
-
-// Cloudinary config
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
 import cloudinary from 'cloudinary';
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
-});
-
-// Constants
-const PORT = process.env.PORT || 5100;
 
 // Router
 import jobRouter from './routes/jobRouter.js';
@@ -39,23 +22,38 @@ import path from 'path';
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
 import { authMiddleware } from './middleware/authMiddleware.js';
 
+// Cloudinary config
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.resolve(__dirname, './public')));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.static(path.resolve(__dirname, './client/dist')));
+app.use(cookieParser());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-// app.get('/api/v1/test', (req, res) => {
-//   res.json({ msg: 'test route' });
-// });
+// Constants
+
+app.get('/api/v1/test', (req, res) => {
+  res.json({ msg: 'test route' });
+});
 
 app.use('/api/v1/jobs', authMiddleware, jobRouter);
 app.use('/api/v1/users', authMiddleware, userRouter);
 app.use('/api/v1/auth', authRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
 // Error handling for unknown routes
@@ -65,6 +63,8 @@ app.use('*', (req, res) => {
 
 // Error handling for existing routes
 app.use(errorHandlerMiddleware);
+
+const PORT = process.env.PORT || 5100;
 
 try {
   await mongoose.connect(process.env.DATABASE_URL);
